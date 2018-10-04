b908d1f1a40280fef14e8387ee62906900983d29
jepusto <jepusto@gmail.com>
2018-10-04 15:01:02 -0500

    Error handling for score tests.

--- R/score-tests.R
+++ R/score-tests.R
@@ -1,3 +1,4 @@
+try_inverse <- function(x) tryCatch(chol2inv(chol(x)), error = function(e) NULL)
 
 VHSM_score_test <- function(model, steps, type = "parametric", info = "expected") {
   
@@ -22,14 +23,18 @@ VHSM_score_test <- function(model, steps, type = "parametric", info = "expected"
     
     S_vec <- null_score(beta, tau_sq, steps, y, s, X, prep = prep)
     
-    Q <- sum(chol2inv(chol(I_mat)) * tcrossprod(S_vec))
+    I_mat_inv <- try_inverse(I_mat)
+    
+    Q <- if (is.null(I_mat_inv)) NA else sum(I_mat_inv * tcrossprod(S_vec))
     
   } else if (type == "subscore") {
     
     S_vec <- null_score(beta, tau_sq, steps, y, s, X, prep = prep)
     omega_index <- length(beta) + 1 + 1:length(steps)
     
-    Q <- sum(chol2inv(chol(I_mat[omega_index, omega_index])) * tcrossprod(S_vec[omega_index]))
+    I_sub_inverse <- try_inverse(I_mat[omega_index, omega_index])
+    
+    Q <- if (is.null(I_sub_inverse)) NA else sum(I_sub_inverse * tcrossprod(S_vec[omega_index]))
     
   } else if (type == "robust") {
     
@@ -38,14 +43,22 @@ VHSM_score_test <- function(model, steps, type = "parametric", info = "expected"
     
     omega_index <- length(beta) + 1 + 1:length(steps)
     S_omega <- S_vec[omega_index]
-    I_model <- I_mat[-omega_index, -omega_index]
-    I_model_omega <- I_mat[omega_index, -omega_index]
-    Bread <- cbind(- I_model_omega %*% chol2inv(chol(I_model)), diag(1L, nrow = q))
-    Meat <- crossprod(S_mat)
+    I_model_inv <- try_inverse(I_mat[-omega_index, -omega_index])
     
-    V_mat <- Bread %*% Meat %*% t(Bread)
-    
-    Q <- sum(chol2inv(chol(V_mat)) * tcrossprod(S_omega))
+    if (is.null(I_model_inv)) {
+      Q <- NA 
+    } else {
+      
+      I_model_omega <- I_mat[omega_index, -omega_index]
+      Bread <- cbind(- I_model_omega %*% I_model_inv, diag(1L, nrow = q))
+      Meat <- crossprod(S_mat)
+      
+      V_mat <- Bread %*% Meat %*% t(Bread)
+      
+      V_inv <- try_inverse(V_mat)
+      
+      Q <- if (is.null(V_inv)) NA else sum(V_inv * tcrossprod(S_omega))
+    }
     
   } else {
     
